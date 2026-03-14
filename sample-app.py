f2e68707ae40b1115d890431a3207238dd969222
BabyniukHanna <babyniuk.hanna@chnu.edu.ua>
2026-03-14 21:51:18 +0000

    Changed port from 8080 to 5050.

--- sample-app.py
+++ sample-app.py
@@ -1,10 +1,8 @@
-# Add to this file for the sample app lab
 from flask import Flask
 from flask import request
 from flask import render_template
 
 sample = Flask(__name__)
-
 @sample.route("/")
 def main():
     return render_template("index.html")
